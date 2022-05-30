278a6eb4ec69038c6003c68f67303b670ba5561f
Vincent9966 <2580411421@qq.com>
2022-05-30 08:37:44 +0800

    调整了格式

---
+++ ThreadPool/ThreadPool.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tanzq on 2022-05-26.
+//
+
+#include "ThreadPool.h"
--- main.cpp
+++ main.cpp
@@ -7,16 +7,14 @@ typedef void (*FunctionPoint)();  // 定义函数指针
 // 定义运行函数id
 enum ERunID{
     SingletonPattern // 单例模式运行代码
-};
+}RunID;
 
-ERunID RunID;
+// 将函数和运行函数ID绑定起来，方便调用
 map<ERunID, FunctionPoint> Functions;
-
 
 void init();
 int main() {
     init();
-    ERunID RunID = ERunID::SingletonPattern;
     return 0;
 }
 
